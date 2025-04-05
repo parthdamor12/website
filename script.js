0cdeff41856f44d22d3d308af34ce4fd1159556f
parthdamor12 <parthdamor855@gmail.com>
2025-04-05 16:21:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,14 @@
-let localConnection;
-let remoteConnection;
-let sendChannel;
-let receiveChannel;
-let localStream;
-let remoteStream;
-let messagesArea = document.getElementById('messages');
-let messageInput = document.getElementById('messageInput');
+// Function to show the Like button and hide other content
+function showLike() {
+    // Hide initial content
+    document.getElementById("content").style.display = "none";
 
-// Function to start a call
-function startCall() {
-    // Setting up the peer-to-peer connection
-    localConnection = new RTCPeerConnection();
-    remoteConnection = new RTCPeerConnection();
-
-    // Setting up the data channels
-    sendChannel = localConnection.createDataChannel("sendDataChannel");
-    sendChannel.onopen = () => console.log("Data channel open");
-    sendChannel.onclose = () => console.log("Data channel closed");
-
-    // Receiving the data on the remote connection
-    remoteConnection.ondatachannel = event => {
-        receiveChannel = event.channel;
-        receiveChannel.onmessage = (event) => {
-            displayMessage(event.data);
-        };
-    };
-
-    // Connecting the two peers
-    localConnection.onicecandidate = e => handleICECandidate(e, remoteConnection);
-    remoteConnection.onicecandidate = e => handleICECandidate(e, localConnection);
-
-    localConnection.createOffer()
-        .then(offer => {
-            return localConnection.setLocalDescription(offer);
-        })
-        .then(() => remoteConnection.setRemoteDescription(localConnection.localDescription))
-        .then(() => remoteConnection.createAnswer())
-        .then(answer => remoteConnection.setLocalDescription(answer))
-        .then(() => localConnection.setRemoteDescription(remoteConnection.localDescription))
-        .catch(error => console.error(error));
+    // Show the like section
+    document.getElementById("likeSection").style.display = "block";
 }
 
-// Function to handle ICE candidates (for P2P connection)
-function handleICECandidate(event, connection) {
-    if (event.candidate) {
-        connection.addIceCandidate(event.candidate);
-    }
-}
-
-// Function to send message
-function sendMessage() {
-    let message = messageInput.value;
-    if (message) {
-        sendChannel.send(message);  // Send the message through the data channel
-        displayMessage(message);    // Display message locally
-        messageInput.value = '';    // Clear the input
-    }
-}
-
-// Function to display received messages
-function displayMessage(message) {
-    messagesArea.value += message + '\n';
-    messagesArea.scrollTop = messagesArea.scrollHeight;
-}
-
-// Function to hang up the call
-function hangUp() {
-    localConnection.close();
-    remoteConnection.close();
-    sendChannel.close();
-    receiveChannel.close();
-    localConnection = null;
-    remoteConnection = null;
-    console.log("Call ended");
+// Function to show the 'like' message after clicking like
+function like() {
+    // Show the "You liked this page!" message
+    document.getElementById("likeMessage").style.display = "block";
 }
